52bbab1929f22c8f1a046b2c88e7038bd0ad0e8a
Janie Tito <jtito@hotmail.com>
2016-11-23 22:49:32 +1300

    sorted files

--- projects/silly.js
+++ projects/silly.js
@@ -9,3 +9,6 @@ function newNameProcess (){
 
 //next, compare the letters to a list of names. Do letter one first.
 //once finished, will come back and add a function to check if the input data is a letter or if not it will ask the user to try again with a letter between a and z
+function useLetters (){
+  
+}
