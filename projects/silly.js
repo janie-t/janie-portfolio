f222b251e55e6d7b45317df67db3d2c4d46ac815
Janie Tito <jtito@hotmail.com>
2016-11-23 22:34:17 +1300

    reworked letters

--- projects/silly.js
+++ projects/silly.js
@@ -3,6 +3,9 @@ function newNameProcess (){
   var firstLetter = document.newNameForm.firstLetter.value;
   var secondLetter = document.newNameForm.secondLetter.value;
   var thirdLetter = document.newNameForm.thirdLetter.value;
-  var nameLetters = '<div>Using these letters: ' + " " + firstLetter + ", " + secondLetter + ", " + thirdLetter  + '</div>';
-  document.getElementById('letters').innerHTML = nameLetters;
+  var nameLetters = '<div>Using these letters?: ' + " " + firstLetter + ", " + secondLetter + ", " + thirdLetter  + '</div>';
+  document.getElementById("letters").innerHTML = nameLetters;
 }
+
+//next, compare the letters to a list of names. Do letter one first.
+//once finished, will come back and add a function to check if the input data is a letter or if not it will ask the user to try again with a letter between a and z
