290ddf8d72764f8aea1b9b19a14ec712a5a4cec7
Janie Tito <jtito@hotmail.com>
2016-11-23 22:04:27 +1300

    letters now show on DOM

--- projects/silly.js
+++ projects/silly.js
@@ -3,6 +3,6 @@ function newNameProcess (){
   var firstLetter = document.newNameForm.firstLetter.value;
   var secondLetter = document.newNameForm.secondLetter.value;
   var thirdLetter = document.newNameForm.thirdLetter.value;
-  var nameLetters = '<div>"Letters:" + firstLetter + secondLetter + thirdLetter </div>';
-  document.getElementById('nameLetters').innerHTML = nameLetters;
+  var nameLetters = '<div>Letters: ' + firstLetter + secondLetter + thirdLetter  + '</div>';
+  document.getElementById('letters').innerHTML = nameLetters;
 }
