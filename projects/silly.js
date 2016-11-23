8a4b4a2bd62fca0d5e353349857486ea4ef55fc0
Janie Tito <jtito@hotmail.com>
2016-11-23 22:01:20 +1300

    got the button to start a function

---
+++ projects/silly.js
@@ -0,0 +1,8 @@
+//first, print the letters that have been entered on the screen
+function newNameProcess (){
+  var firstLetter = document.newNameForm.firstLetter.value;
+  var secondLetter = document.newNameForm.secondLetter.value;
+  var thirdLetter = document.newNameForm.thirdLetter.value;
+  var nameLetters = '<div>"Letters:" + firstLetter + secondLetter + thirdLetter </div>';
+  document.getElementById('nameLetters').innerHTML = nameLetters;
+}
